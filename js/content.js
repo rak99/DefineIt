a3520e6f1f769f611420e3822217b56c02056269
R.Kloppers <rhenierkloppers124@gmail.com>
2019-10-09 14:14:11 +0200

    if popup's height is higher than height left on the page, thus having to expand the page to show it, it now gets popped up on top rather than bottom, also fixed a bug with the overlapWidth not working properly where popup doesn't go more left even though it should've, need to still decide on width and height, make it by default a decent general size and make it resizeable by users, moved some functions inside the 404 checker to prevent unnecessary execution of code when popup shouldn't even show, fixed where if a non text node was selected and it had whitepsace in it, an error would occur with range.surroundContents(), using dom manipulation to make the selected text bold, and then look at that rather than the bold with range.surroundContents - also had an error where if I changed outerHTML it would deselect the word selected and only leave the whitespace, using a function to re-select the word afterwards, need to still take the bald away after click, not really necessary but probably a good idea

--- js/content.js
+++ js/content.js
@@ -16,6 +16,39 @@ let boldPosition = '';
 let overlapWidth = 0;
 let contextMenuExists = false;
 
+
+// Find element's position relative to the document (so if scrolled down this is very useful)
+function getCoords(elem) { // crossbrowser version
+    var box = elem.getBoundingClientRect();
+
+    var body = document.body;
+    var docEl = document.documentElement;
+
+    var scrollTop = window.pageYOffset || docEl.scrollTop || body.scrollTop;
+    var scrollLeft = window.pageXOffset || docEl.scrollLeft || body.scrollLeft;
+
+    var clientTop = docEl.clientTop || body.clientTop || 0;
+    var clientLeft = docEl.clientLeft || body.clientLeft || 0;
+
+    var top  = box.top +  scrollTop - clientTop;
+    var left = box.left + scrollLeft - clientLeft;
+    return { top: Math.round(top), left: Math.round(left) };
+}
+
+function selectElement(element) {
+    if (window.getSelection) {
+        var sel = window.getSelection();
+        sel.removeAllRanges();
+        var range = document.createRange();
+        range.selectNodeContents(element);
+        sel.addRange(range);
+    } else if (document.selection) {
+        var textRange = document.body.createTextRange();
+        textRange.moveToElementText(element);
+        textRange.select();
+    }
+}
+
 // !-- MAKE SURE NOT TO SEARCH FOR NUMBERS
 /* 
 document.addEventListener('scroll', function(e) {
@@ -33,8 +66,29 @@ window.oncontextmenu = function () {
 
 function getSelectionText(e) {
 
+    var body = document.body,
+    html = document.documentElement;
+
+    var documentHeight = Math.max( body.scrollHeight, body.offsetHeight, 
+        html.clientHeight, html.scrollHeight, html.offsetHeight 
+        );
+    var documentWidth = Math.max( body.scrollWidth, body.offsetWidth,
+        html.clientWidth, html.scrollWidth, html.offsetWidth
+         );
+
     // !-- TODO - Add scroll to base feature
     // !-- Some selected words are not in their origin/base form, e.g playing is selected, but the popup website I use, Merriam-Webster, starts at the origin/base word
+    // !-- ALSO, if the popupHeight is higher than the bottom left on the page, make popup go top rather than bottom
+    // !-- So check
+    // !!!-- Top Done --!!!
+    // !-- Now, overLapWidth thingy isn't working anymore, fix it
+    // !-- I belive Top is also fixed, nice.
+    // !-- Fix non text only nodes selecting space and it causing an error
+    // !-- Fixed width and height based on media query
+    // !-- New idea is to make iframe resizeable by user, size gets saved to chrome.storage.sync or localStorage on background.js on mouseup it gets saved, 
+    // !-- then fetch it from there if it exists
+    // !-- Also want to add a popup that lets you specific page or whole href, also add unblacklist if site is blacklisted think I said it here somewhere already
+    // !-- Maybe add a keybind to it 
     // in this case, play. Maybe find a way to search for the word 'Play' by searching for indexOf('Play') and the nodeName has to be an <h1> or something,
     // do getBoundingClientRect() on it and scroll the popup down there, I'm pretty sure it's easy to do.
 
@@ -45,43 +99,87 @@ function getSelectionText(e) {
     
             // Rework to use x and y rather than document I think... 
             text = window.getSelection().toString().trim();
+            console.log(text);
             let selectedDOM = window.getSelection().focusNode;
             // !-- This below method may be inconsistent if there are more versions of the dom element surrounding the selection, this method searches the entire body for the text, and assumes it appears only once.
             // Use below as reference, with the emphasis on range as it allows dom insertion at the specific select.
             let highlight = window.getSelection(),
             bold = document.createElement('b'),
             range = highlight.getRangeAt(0);
-            if (range.startContainer.nextSibling) {
-                if (range.startContainer.nextSibling.toString() !== "[object Text]") {
+            bold.style.fontWeight = 'unset'
+/*             if (range.startContainer.nextSibling) {
+                console.log()
+                if (range.startContainer.nextSibling.nodeName.toString() !== "[object Text]") {
                     boldPosition = range.startContainer.nextSibling.getBoundingClientRect();
+                } else {
+                    range.surroundContents(bold);
+                    boldPosition = bold.getBoundingClientRect();
                 }
             } else {
+                range.surroundContents(bold);
                 boldPosition = bold.getBoundingClientRect();
-            }
-    
+            } */
+            // bold.getBoundingClientRect();
+            
             // Begin popup ---
             let popupNode = document.createElement('iframe');
-            popupNode.src = `https://www.merriam-webster.com/dictionary/${range}`;
+            popupNode.src = `https://www.merriam-webster.com/dictionary/${text}`;
             fetch(popupNode.src)
             .then((res) => {
                 if (res.status !== 404) {
+                    console.log(range.commonAncestorContainer.nodeName !== "P");
+                    if (range.commonAncestorContainer.nodeName !== "P") {
+                        range.surroundContents(bold);
+                        boldPosition = getCoords(bold);
+                    } else {
+                        console.log(range);
+                        let selectedText = window.getSelection().toString().trim();
+                        let selectedTextLength = window.getSelection().toString().trim().length;
+                        console.log(selectedText);
+                        let nodeOuterHTML = range.startContainer.nextSibling.outerHTML;
+                        console.log(nodeOuterHTML);
+                        console.log(nodeOuterHTML.indexOf(selectedText));
+                        let selectedTextIndex = nodeOuterHTML.indexOf(selectedText);
+                        let selectedTextToBold = `<b id="DefineItTextToBold">${selectedText}</b>`;
+                        range.startContainer.nextSibling.outerHTML = nodeOuterHTML.slice(0, selectedTextIndex) + 
+                        selectedTextToBold + 
+                        nodeOuterHTML.slice(selectedTextIndex + selectedTextLength, nodeOuterHTML.length);
+                        console.log(nodeOuterHTML);
+                        let newBoldElement = document.getElementById('DefineItTextToBold');
+                        boldPosition = getCoords(newBoldElement);
+                        selectElement(newBoldElement);
+                        console.log(boldPosition);
+                    }
+                    // !-- Decide if we want to make height and width not a constant, atm getting height and width at this stage doesn't work, what we could do is define
+                    // !-- it with js, that we can access it here, problem is media queries work nicely  
                     popupNode.className = "selectedWord";
                     // Styling
                     popupNode.id = 'defineIt-popupNode';
-                    range.insertNode(popupNode);
-                    console.log(popupNode.getBoundingClientRect());
-                    overlapWidth = $(document).width() - popupNode.getBoundingClientRect().x;
+                    // !-- Old way that gets cropped --!
+                    // range.insertNode(popupNode);
+                    document.getElementsByTagName('body')[0].insertBefore(popupNode, document.getElementsByTagName('body')[0].firstChild);
+                    let popupNodePositionStart = popupNode.getBoundingClientRect();
+                    let popupNodeLeftToRightStart = popupNodePositionStart.x + popupNodePositionStart.width;
+                    let popupNodeTopToBottomStart = popupNodePositionStart.y + popupNodePositionStart.height;
+                    popupNode.style.left = (boldPosition.left) + 'px';
+                    let overlapHeight = boldPosition.top - documentHeight + 400;
+                    if ( (boldPosition.top - documentHeight + 400) > -18) {
+                        // Trigger from top
+                        popupNode.style.top = boldPosition.top - 415 - 25 + 'px';
+                    } else {
+                        popupNode.style.top = boldPosition.top - 15 + 'px';
+                    }
+                    overlapWidth = documentWidth - popupNode.getBoundingClientRect().x;
                     let rangeNode = range.startOffset;
-                    if ( overlapWidth < 426 ) {
+                    if ( overlapWidth < 326 ) {
                             if (document.getElementById('defineIt-popupNode')) {
-                                popupNode.style.right = 426 - overlapWidth + 'px';
+                                popupNode.style.left = parseInt(popupNode.style.left) - (326 - overlapWidth) + 'px';
                             }
                     }
                     const showPopup = (e) => {
                         if (e.which === 1) {
                             // !-- Remove if else later if I decide a single click outside popup should close with contextMenu open
                             if (contextMenuExists === true) {
-                                console.log('next click will close');
                                 contextMenuExists = false;
                             } else {
                                 if ( document.getElementById('defineIt-popupNode') ) {
@@ -90,11 +188,12 @@ function getSelectionText(e) {
                                     let popupNodeTopToBottom = popupNodePositions.y + popupNodePositions.height;
                                     var x = event.clientX;     // Get the horizontal coordinate
                                     var y = event.clientY;     // Get the vertical coordinate
+                                    $(bold).contents().unwrap();
                                     var coor = "X coords: " + x + ", Y coords: " + y;
                                     if ( ( x < (popupNodePositions.x) || x > popupNodeLeftToRight ) || y < (popupNodePositions.y) || y > popupNodeTopToBottom ) {
                                         // range.startContainer.childNodes[rangeNode].remove();
                                         document.getElementById('defineIt-popupNode').remove();
-                                        $(bold).contents().unwrap();
+                                        
                                         window.removeEventListener('mousedown', showPopup, false);
                                         let popupNodePosition = popupNode.getBoundingClientRect();
                                         popupNode.style.left = (boldPosition.left - popupNodePosition.left + 'px') + popupNode.style.width;
@@ -106,8 +205,14 @@ function getSelectionText(e) {
                         }
                     };
                     window.addEventListener('mousedown', showPopup, false);
-                    $(bold).contents().unwrap();
+                    //$(bold).contents().unwrap();
                     // End popup ---
+                } else {
+                    window.onmousedown = function(e) {
+                        if (e.which === 1) {
+                            $(bold).contents().unwrap();
+                        }
+                    } 
                 }
             });
         } else if (document.selection && document.selection.type != "Control" && document.selection.createRange().text.length > 0) {
@@ -129,7 +234,8 @@ function getSelectionText(e) {
         return text;
     }
 }
-window.addEventListener('mouseup', e => {
+
+window.onmouseup = function(e) {
     stopFunction = false;
     getSelectionText(e);
-});
+}
