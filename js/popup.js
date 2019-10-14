9433ef9938f91786cc84e46fbac5344379542999
R.Kloppers <rhenierkloppers124@gmail.com>
2019-10-14 20:09:15 +0200

    reconfiguring blacklist

--- js/popup.js
+++ js/popup.js
@@ -1,7 +1,5 @@
 console.log('popup active');
 let currentURL = '';
-var manifest = chrome.runtime.getManifest();
-console.log(manifest.content_scripts[0].exclude_matches);
 let blacklists = '';
 
 chrome.storage.local.get(['blacklistedURLS'], function(res) {
@@ -19,20 +17,98 @@ chrome.storage.local.get(['currentURL'], function(result) {
     console.log(currentURL.currentURL.url);
     // Use clever tactic to extract hostname from url by turning it into a a element, setting the href attribute to the url we want
     // the a element hasa built in hostname property that extracts it from the href, so all that's left is to use a.hostname and voila!
+
+    // 2 Statements to be met, which button is clicked, and if it's clicked, change contents of popop accordingly
+
     let a = document.createElement('a');
     let url = currentURL.currentURL.url;
+    // url is the entire url, so this would be used to exclude the singular current page the user is on
+    // in turn, this is the hostname of the url, for example www.wikipedia.org/*, so it would exclude all apges on this domain. 
     a.href = url;
-    let hostnameUrl = a.hostname;
+    let hostnameUrl = a.hostname + '/*';
     let excludeArr = blacklists;
-    let popopPrefixEl = document.getElementsByClassName('defineIt-blacklist-popup-prefix');
+    let popupPrefixEl = document.getElementsByClassName('defineIt-blacklist-popup-prefix');
     let siteSpecificEl = document.getElementById('blacklist-site-specific');
     let siteEntireEl = document.getElementById('blacklist-site-entire');
+    let containerEl = document.getElementsByClassName('defineIt-blacklist-container');
+
+    function checkIfCurrentURLIsBlacklisted(url) {
+        return blacklists.indexOf(url) > -1;
+    }
+
+    let isDomainBlacklisted = checkIfCurrentURLIsBlacklisted();
+    let isPageBlacklisted = checkIfCurrentURLIsBlacklisted();
+
+    if (isDomainBlacklisted === true || isPageBlacklisted === true) {
+        // URL or domain is blacklisted
+        // Do Un-Blacklist related things here
+        // Change text as well, maybe 'Un-Blacklist This Page'
+        if (containerEl.length > 0) {
+            if (isDomainBlacklisted === true && isPageBlacklisted === true) {
+
+                for (let i = 0; i < popupPrefixEl.length; i++) {
+                    popupPrefixEl[i].textContent = 'Un-Blacklist';
+                }
+                siteSpecificEl.textContent = 'This Specific Page';
+                siteEntireEl.textContent = hostnameUrl;
+
+            } else if (isDomainBlacklisted === true) {
+                containerEl[0].remove();
+                popupPrefixEl[0].textContent = 'Un-Blacklist';
+                siteSpecificEl.textContent = hostnameUrl;
+            } else {
+                // isPageBlackListed === true
+                containerEl[1].remove();
+                popupPrefixEl[1].textContent = 'Un-Blacklist';
+                siteSpecificEl.textContent = url;
+            }
+        }
+
+    } else {
+        // URL or domain not blacklisted
+        // Do blacklist related things here
+        for (let i = 0; i < popupPrefixEl.length; i++) {
+            popupPrefixEl[i].textContent = 'Blacklist';
+        }
+        siteSpecificEl.textContent = 'This Specific Page';
+        siteEntireEl.textContent = hostnameUrl;
+    }
+
+    if (siteSpecificEl) { 
+
+        function sliceAndConcatArray(arr, indexOfItemToBeRemoved) {
+            // Give 2 arrays that you want to remove items from and return the removed array
+            if (arr) {
+                if (typeof indexOfItemToBeRemoved !== 'number') 
+                    return console.error('error, number param is not a number');
+                else {
+                    let fromArrBeginTillMatch = arr.slice(0, indexOfItemToBeRemoved);
+                    let fromArrExcludeTillEnd = arr.slice(indexOfItemToBeRemoved+1, arr.length);
+                    return fromArrBeginTillMatch.concat(fromArrExcludeTillEnd);
+                }
+            }
+        };
+
+        function popupHandler(url) {
+            let indexOfItemToBeRemoved = blacklists.indexOf(url);
+            let reassembledArray = sliceAndConcatArray(blacklists, indexOfItemToBeRemoved);
+        };
+
+        siteSpecificEl.addEventListener('click', function() {
+            popupHandler(url);
+        }, false);
+
+        siteEntireEl.addEventListener('click', function() {
+            popupHandler(hostnameUrl);
+        }, false);
+    }
+
     if (siteSpecificEl) {
         let excludeArrIndex = excludeArr.indexOf(url)
         if (excludeArrIndex !== -1) {
             // If found
-            if (popopPrefixEl.length > 0) {
-                for (let elt of popopPrefixEl) {
+            if (popupPrefixEl.length > 0) {
+                for (let elt of popupPrefixEl) {
                     elt.textContent = 'Un-Blacklist';
                 }
             }
@@ -42,13 +118,11 @@ chrome.storage.local.get(['currentURL'], function(result) {
                 let fromArrBeginTillMatch = excludeArr.slice(0, excludeArrIndex);
                 let fromArrExcludeTillEnd = excludeArr.slice(excludeArrIndex+1, excludeArr.length);
                 let concatedExcludeArr = fromArrBeginTillMatch.concat(fromArrExcludeTillEnd);
-                manifest.content_scripts[0].exclude_matches = concatedExcludeArr;
-                console.log(manifest.content_scripts[0].exclude_matches);
             });
         } else {
             // If not found
-            if (popopPrefixEl.length > 0) {
-                for (let elt of popopPrefixEl) {
+            if (popupPrefixEl.length > 0) {
+                for (let elt of popupPrefixEl) {
                     elt.textContent = 'Blacklist';
                 }
             }
@@ -66,8 +140,8 @@ chrome.storage.local.get(['currentURL'], function(result) {
         let excludeArrIndex = excludeArr.indexOf(hostnameUrl + '/*');
         if (excludeArrIndex !== -1) {
             // If found
-            if (popopPrefixEl.length > 0) {
-                for (let elt of popopPrefixEl) {
+            if (popupPrefixEl.length > 0) {
+                for (let elt of popupPrefixEl) {
                     elt.textContent = 'Un-Blacklist';
                 }
             }
@@ -82,8 +156,8 @@ chrome.storage.local.get(['currentURL'], function(result) {
             }); 
         } else {
             // If not found
-            if (popopPrefixEl.length > 0) {
-                for (let elt of popopPrefixEl) {
+            if (popupPrefixEl.length > 0) {
+                for (let elt of popupPrefixEl) {
                     elt.textContent = 'Blacklist';
                 }
             }
